42100711b1b584e4f1ecb131e025fba442c89acc
devops2517 <nirvana.karmazina@gmail.com>
2023-05-13 17:19:31 +0300

    all

--- main.tf
+++ main.tf
@@ -2,22 +2,51 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "instance" {
-  source = "./instance"
-  count = 2
+resource "aws_security_group" "allow_ssh" {
+  name_prefix = "allow_ssh"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_instance" "instance" {
-  ami           = "ami-0c94855ba95c71c99"
+resource "aws_instance" "build" {
+  ami = "ami-0c78505fac59e4c3c"
   instance_type = "t2.micro"
-  tags = {
-    Name = "boxfuse-instance-${count.index + 1}"
-  }
+  key_name = "my_key_pair"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install git -y
+              git clone https://github.com/boxfuse/boxfuse-sample-java-war-hello.git
+              cd boxfuse-sample-java-war-hello
+              ./mvnw package aws-maven -DartifactId=hello -Dversion=1.0 -Denvironment=prod -Daws.region=${var.region}
+              aws s3 cp target/hello-1.0.war s3://${var.bucket_name}/hello-1.0.war
+              EOF
+}
 
-provisioner "remote-exec" {
-     inline = [
-       "sudo yum update -y",
-       "sudo yum install -y git java-1.8.0-openjdk",
-     ]
-   }
- }
+resource "aws_instance" "app" {
+  ami = "ami-0c78505fac59e4c3c"
+  instance_type = "t2.micro"
+  key_name = "my_key_pair"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install tomcat tomcat-webapps -y
+              aws s3 cp s3://${var.bucket_name}/hello-1.0.war /usr/share/tomcat/webapps/hello.war
+              sudo systemctl enable tomcat
+              sudo systemctl start tomcat
+              EOF
+}
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "bucket_name" {
+  default = "terraform-example-bucket"
+}

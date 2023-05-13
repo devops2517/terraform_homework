7e0411cb1ebc1c5ab64079a58161757b3119d3fb
devops2517 <nirvana.karmazina@gmail.com>
2023-05-13 18:16:49 +0300

    part2_all

--- part_two.tf
+++ part_two.tf
@@ -7,20 +7,31 @@ resource "aws_key_pair" "my_key" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "aws_instance" "VM" {
-  ami = "ami-0aa2b7722dc1b5612"
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.my_key.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-root_block_device {
-    volume_size = "${var.root_volume_size}"
-}
+resource "aws_instance" "VM" {
+  ami           = "ami-0aa2b7722dc1b5612"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  root_block_device {
+    volume_size = var.root_volume_size
+  }
 
   ebs_block_device {
     device_name = "/dev/sdb"
-    volume_size = "${var.data_volume_size}"
+    volume_size = var.data_volume_size
+  }
 }
 
 variable "instance_type" {
@@ -28,9 +39,9 @@ variable "instance_type" {
 }
 
 variable "root_volume_size" {
-  default = "8"
+  default = 8
 }
 
 variable "data_volume_size" {
-  default = "20"
+  default = 20
 }

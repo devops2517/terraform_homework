2ba4ac2f588aed9aaa268dce13e7a0d856fcd98d
devops2517 <nirvana.karmazina@gmail.com>
2023-05-13 18:12:02 +0300

    part2

---
+++ part_two.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "my_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "VM" {
+  ami = "ami-0aa2b7722dc1b5612"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+}
+
+root_block_device {
+    volume_size = "${var.root_volume_size}"
+}
+
+  ebs_block_device {
+    device_name = "/dev/sdb"
+    volume_size = "${var.data_volume_size}"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "root_volume_size" {
+  default = "8"
+}
+
+variable "data_volume_size" {
+  default = "20"
+}
